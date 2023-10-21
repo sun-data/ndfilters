80264c29eaec953cf789b257d4b9bf1d65600fa8
Roy Smart <roytsmart@gmail.com>
2023-10-21 09:18:31 -0600

    Moved project location to the `sun-data` organization.

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ html_theme_options = {
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "https://github.com/byrdie/ndfilters",
+            "url": "https://github.com/sun-data/ndfilters",
             "icon": "fa-brands fa-github",
             "type": "fontawesome",
         },
