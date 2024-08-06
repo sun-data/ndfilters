adbcb3529979c09690f5833eb44a3615bdc15eaf
Roy Smart <roytsmart@gmail.com>
2024-08-05 20:15:37 -0600

    docs

--- ndfilters/__init__.py
+++ ndfilters/__init__.py
@@ -1,3 +1,7 @@
+"""
+Numba-accelerated, n-dimensional filters similar to those in :mod:`scipy.ndimage`.
+"""
+
 from ._generic import generic_filter
 from ._mean import mean_filter
 from ._trimmed_mean import trimmed_mean_filter
