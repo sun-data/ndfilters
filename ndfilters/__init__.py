d4db1728d2937f4ba6bf8db6507b951fb5b532a9
Roy Smart <roytsmart@gmail.com>
2025-03-30 17:24:33 -0500

    Added `ndfilters.median_filter()` function. (#24)

--- ndfilters/__init__.py
+++ ndfilters/__init__.py
@@ -5,6 +5,7 @@ Numba-accelerated, n-dimensional filters similar to those in :mod:`scipy.ndimage
 from ._generic import generic_filter
 from ._mean import mean_filter
 from ._trimmed_mean import trimmed_mean_filter
+from ._median import median_filter
 from ._variance import variance_filter
 from ._convolve import convolve
 
@@ -12,6 +13,7 @@ __all__ = [
     "generic_filter",
     "mean_filter",
     "trimmed_mean_filter",
+    "median_filter",
     "variance_filter",
     "convolve",
 ]
